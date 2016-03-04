77eb549e9e44aed585c59009b7413f37a7ffbe03
Sam Andersen <samstext@gmail.com>
2016-03-04 09:56:16 -0500

    Begin porting to C

---
+++ enableChromiumFlash.c
@@ -0,0 +1,19 @@
+/*
+ * Project: enableChromiumFlash
+ * Author: Sam Andersen
+ * Email: samstext@gmail.com
+ * Version: 20160304
+ *
+ * enableChromiumFlash: small utility for enabling the PPAPI flash plugin
+ * for Chromium on OS X. 
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "enableChromiumFlash.h"
+
+int main(void) {
+	int x;
+	return 0;
+} 
---
+++ enableChromiumFlash.h
@@ -0,0 +1,20 @@
+/*
+ * Project: enableChromiumFlash
+ * Author: Sam Andersen
+ * Email: samstext@gmail.com
+ * Version: 20160304
+ *
+ * enableChromiumFlash: small utility for enabling the PPAPI flash plugin
+ * for Chromium on OS X. 
+ *
+ */
+
+#ifndef ENABLEFLASH_H
+#define ENABLEFLASH_H
+
+typedef enum {
+	true = 0,
+	false = 1
+} bool;
+
+#endif
