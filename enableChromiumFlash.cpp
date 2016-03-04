eecf6b9506c75af071b1a5d19c921ce2867d2e33
Sam Andersen <samstext@gmail.com>
2016-03-04 09:55:52 -0500

    Begin porting to C

--- enableChromiumFlash.cpp
+++ enableChromiumFlash.cpp
@@ -3,6 +3,51 @@
 #include "ioclass.cpp"
 #include "systemClass.cpp"
 
+class io {
+public:
+    void requestVersion(std::string* versionNumber) {
+        std::cout << "Enter a Flash version number: ";
+        std::cin >> *versionNumber;
+    }
+    void createFile(const std::string* executionString) {
+        std::ofstream sourceFile;
+        sourceFile.open("chromium.cpp");
+        sourceFile << "#include <iostream>\n";
+        sourceFile << "int main() {\n";
+        sourceFile << *executionString;
+        sourceFile << "return 0;\n";
+        sourceFile << "}\n";
+        sourceFile.close();
+        
+    }
+    bool verifyInstallation() {
+        std::cout << "This version of Chromium has not yet been patched for Flash. Do you want to continue? [Y/N]: ";
+        std::cin >> vInstall;
+        if (vInstall == 'Y' || vInstall == 'y') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+private:
+    char vInstall;
+} io;
+
+class systemFunctions {
+public:
+    bool existingInstallation(std::string* chromeName) {
+        std::ifstream infile(*chromeName);
+        return infile.good();
+    }
+    void createInstallation() {
+        system("mv /Applications/Chromium.app/Contents/MacOS/Chromium /Applications/Chromium.app/Contents/MacOS/Chromium_");
+    }
+    void enableFlash() {
+        system("clang++ -Wall -Wextra -Wpedantic -std=c++14 chromium.cpp -o /Applications/Chromium.app/Contents/MacOS/Chromium");
+    }
+} sysF;
+
 int main() {
     
     std::string versionNumber;
